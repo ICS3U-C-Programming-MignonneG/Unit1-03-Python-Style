c1f58b98da142a555de46212ab4141b8a90f483e
Mignonne Gihozo <Mignonne.Gihozo@stu.ocsb.ca>
2026-09-15 13:50:41 +0000

    First commit of hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Created By: Mignonne Gihozo
+# Date: Sep. 15, 2026
+# This program display a greeting and contains proper formatting .
+def main():
+    print("Hello, Mignonne!")
+    print("programming is fun!")
+
+
+if __name__ == "__main__":
+    main()
